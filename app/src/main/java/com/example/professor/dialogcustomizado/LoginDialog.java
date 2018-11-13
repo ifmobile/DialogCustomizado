4da5d5b80f3f5d7fb0f8aa24a8936ab1e10505fa
IFMobile <ifmobilefoz@gmail.com>
2018-11-13 21:41:48 -0200

    Modificações para versão 2

--- app/src/main/java/com/example/professor/dialogcustomizado/LoginDialog.java
+++ app/src/main/java/com/example/professor/dialogcustomizado/LoginDialog.java
@@ -1,6 +1,7 @@
 package com.example.professor.dialogcustomizado;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,12 +17,14 @@ public class LoginDialog extends AppCompatDialogFragment
 
     private EditText edtLogin;
     private EditText edtSenha;
+    private OnLoginListener listener;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setTitle(R.string.titleDialog);
+
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View layout = inflater.inflate(R.layout.dialog_login, null);
@@ -39,10 +42,10 @@ public class LoginDialog extends AppCompatDialogFragment
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        String login = edtLogin.getText().toString();
-        String senha = edtSenha.getText().toString();
 
         if(which == DialogInterface.BUTTON_POSITIVE){
+            String login = edtLogin.getText().toString();
+            String senha = edtSenha.getText().toString();
 
             if (login.equals("login") && senha.equals("123456")){
                 Intent intent = new Intent(getActivity(),LoginActivity.class);
@@ -52,11 +55,34 @@ public class LoginDialog extends AppCompatDialogFragment
             else
                 Toast.makeText(getActivity(), R.string.loginFail,
                         Toast.LENGTH_SHORT).show();
-
+                if(listener != null)
+                    listener.onLogin(login,senha);
         }
         else if (which == DialogInterface.BUTTON_NEGATIVE){
             Toast.makeText(getActivity(), R.string.loginCancel,
                     Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (!(context instanceof OnLoginListener)) {
+            throw new IllegalArgumentException("A activity deve implementar LoginDialog.OnLoginListener");
+        }
+        this.listener = (OnLoginListener) context;
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface OnLoginListener{
+        public void onLogin(String login, String senha);
+    }
+
+
 }
--- app/src/main/java/com/example/professor/dialogcustomizado/MainActivity.java
+++ app/src/main/java/com/example/professor/dialogcustomizado/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginDialog.OnLoginListener {
+
+    private TextView txtLogin;
+    private TextView txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
                 loginDialog.show(getSupportFragmentManager(), "loginDialog");
             }
         });
+
+        txtLogin = (TextView) findViewById(R.id.txtLogin);
+        txtSenha = (TextView) findViewById(R.id.txtSenha);
+    }
+
+    @Override
+    public void onLogin(String login, String senha) {
+        txtLogin.setText("Login: " + login);
+        txtSenha.setText("Senha: " + senha);
     }
 }
